569e311a51982c5f255173bbee9ac309ffb1c3f6
Lucas <lucasmteixeira97@gmail.com>
2026-05-04 23:04:18 -0300

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = ENV["FORCE_SSL"].present?
 
+  # Allow requests from the production VPS hostname.
+  config.hosts << "lucasmt.vps-kinghost.net"
+
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
@@ -77,6 +80,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.hosts << "lucasmt.vps-kinghost.net"
+
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
